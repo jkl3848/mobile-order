41ebd01d8bb07d90fccda52baa68e05115023e0f
jlamoreaux <jlamoreaux@bethel.jw.org>
2023-05-02 19:49:37 -0400

    main login page

---
+++ data/loginInfo.js
@@ -0,0 +1,22 @@
+const logins = [
+  {
+    "user": "admin",
+    "password": "admin",
+    "id": 0,
+    "type": "admin"
+  },
+  {
+    "user": "food",
+    "password": "null",
+    "id": 1,
+    "type": "food"
+  },
+  {
+    "user": "event",
+    "password": "null",
+    "id": 1,
+    "type": "event"
+  }
+]
+
+export {logins}
---
+++ js/login.js
@@ -0,0 +1,28 @@
+import { logins } from "../data/loginInfo.js";
+
+var loginButton = document.getElementById("login-button");
+
+loginButton.addEventListener("click", verifyLogin);
+
+function verifyLogin() {
+  var username = document.getElementById("login-field-username").value;
+  var password = document.getElementById("login-field-password").value;
+  var userType = "";
+  var id;
+
+  console.log(username);
+  console.log(password);
+
+  var userInfo = logins.find((obj) => obj.user === username);
+
+  console.log(userInfo);
+  if (userInfo.password === password) {
+    id = userInfo.id;
+    userType = userInfo.type;
+
+    var loginPath = `../vendor/${userType}/vendormain.html`;
+
+    window.location.href = loginPath;
+  } else {
+  }
+}
