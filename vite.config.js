0e925002e9713102fd3a751252ddc452bc509446
jlamoreaux <jlamoreaux@bethel.jw.org>
2023-05-04 16:56:40 -0400

    test

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,7 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   base: "/dist",
-  plugins: [
-    vue({
-      template: {
-        transformAssetUrls: {
-          base: null,
-          includeAbsolute: false,
-        },
-      },
-    }),
-  ],
+  plugins: [vue({})],
   // resolve: {
   //   alias: {
   //     vue: "vue/dist/vue.esm-bundler.js",
