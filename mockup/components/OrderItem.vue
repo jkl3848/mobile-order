096e0b6e0461135599e37ba798696187bc1afcdc
jlamoreaux <jlamoreaux@bethel.jw.org>
2023-05-04 15:44:23 -0400

    vendor orders and items

---
+++ mockup/components/OrderItem.vue
@@ -0,0 +1,64 @@
+<script setup>
+
+const props = defineProps({
+    order: Object
+})
+
+function startOrder() {
+    props.order.status = "started"
+}
+
+function completeOrder() {
+    props.order.status = "complete"
+
+}
+</script>
+<template>
+    <div id="order-details"><span id="order-item">{{ order.item
+    }}</span><span id="order-time">{{ order.orderTime
+}}</span></div>
+    <div>
+        <button class="order-button" id="start-button" @click="startOrder()">Start</button>
+        <button class="order-button" id="complete-button" @click="completeOrder()">Complete</button>
+    </div>
+</template>
+<style scoped>
+#order-details {
+    width: 100%;
+    text-align: left;
+    font-size: 50px;
+    padding-left: 20px;
+
+    display: flex;
+    flex-direction: row;
+
+}
+
+.order-button {
+    color: white;
+    height: 40px;
+    width: 250px;
+    margin-left: 15px;
+    margin-right: 15px;
+    border-radius: 12px;
+    font-size: 20px;
+}
+
+#start-button {
+    background-color: rgb(48, 92, 195);
+    border: 3px solid rgb(30, 58, 124);
+}
+
+#complete-button {
+    background-color: rgb(38, 176, 84);
+    border: 3px solid rgb(24, 109, 52);
+}
+
+.order-item {
+    justify-content: left;
+}
+
+.order-time {
+    justify-content: right;
+}
+</style>
--- mockup/components/VendorPortal.vue
+++ mockup/components/VendorPortal.vue
@@ -1,9 +1,106 @@
 <script setup>
-var test = null;
+import OrderItem from "./OrderItem.vue"
+
+var test = [
+  {
+    "orderNumber": "00001",
+    "item": "food",
+    "name": "Steve",
+    "number": "100",
+    "status": "started",
+    "orderTime": "5:00"
+  },
+
+  {
+    "orderNumber": "00002",
+    "item": "food",
+    "name": "John",
+    "number": "100",
+    "status": "ordered",
+    "orderTime": "5:01"
+  },
+
+  {
+    "orderNumber": "00003",
+    "item": "drink",
+    "name": "Ted",
+    "number": "100",
+    "status": "ordered",
+    "orderTime": "5:04"
+  }
+]
+
+function sortedItemsList(list, isStarted) {
+  var finalList
+
+  if (!isStarted) {
+    finalList = list.filter((item) => {
+      return item.status === "ordered"
+    })
+  } else {
+    finalList = list.filter((item) => {
+      return item.status === "started"
+    })
+  }
+
+  console.log(finalList)
+
+  return finalList
+}
+
+
+function orderIsStarted(item) {
+  if (item.status === "started") {
+    return true
+  }
+  return false
+}
 </script>
 
 <template>
-  <div>Hello World</div>
+  <div id="content">
+    <div id="header">
+      Welcome Vendor
+    </div>
+
+    <div class="order-list" id="in-progress-orders">
+      Orders in Progress
+      <div v-for="item in sortedItemsList(test, true)" class="order-item-obj"
+        :class="orderIsStarted(item) ? 'item-started' : 'item-not-started'">
+        <OrderItem :order="item" />
+      </div>
+    </div>
+
+    <div class="order-list" id="orders">
+      Orders
+      <div v-for="item in sortedItemsList(test, false)" class="order-item-obj">
+        <OrderItem :order="item" />
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+#content {
+  text-align: center;
+  margin: auto;
+  width: 80%;
+  background-color: white;
+  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
+}
+
+.order-list {
+  margin-top: 50px;
+}
+
+.order-item-obj {
+  width: 100%;
+  border: 5px solid gray;
+  border-radius: 15px;
+  padding: 5px
+}
+
+.item-started {
+  border-color: blue
+}
+</style>
