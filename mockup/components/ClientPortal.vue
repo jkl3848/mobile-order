365b1c56f54ec08c8a7dac2c6f5d3af6fe702cf7
jlamoreaux <jlamoreaux@bethel.jw.org>
2023-05-07 19:08:38 -0400

    formats, styles, etc

--- mockup/components/ClientPortal.vue
+++ mockup/components/ClientPortal.vue
@@ -4,13 +4,34 @@ const orderItemList = $ref([
     {
         "itemName": "Cheeseburger",
         "itemNumber": "01",
-        "imagePath": "../mockup/images/cheeseburger.jpg"
+        "imagePath": "../mockup/images/cheeseburger.jpg",
+        "selected": false
     }, {
         "itemName": "Drink",
         "itemNumber": "02",
-        "imagePath": "../mockup/images/drink.jpg"
+        "imagePath": "../mockup/images/drink.jpg",
+        "selected": true
+    },
+    {
+        "itemName": "Secret Item",
+        "itemNumber": "03",
+        "imagePath": "null",
+        "selected": false
     }
 ])
+
+function selectItem(itemIndex) {
+    var skip = false
+    if (orderItemList[itemIndex].selected == true) {
+        skip = true
+    }
+    for (let i = 0; i < orderItemList.length; i++) {
+        orderItemList[i].selected = false
+    }
+    if (!skip) {
+        orderItemList[itemIndex].selected = true
+    }
+}
 </script>
 
 <template>
@@ -20,16 +41,38 @@ const orderItemList = $ref([
         </div>
         <div id="form">
             <label class="user-input-label">Item: </label>
-            <div class="item-select" v-for="item in orderItemList" :key="item.itemNumber">
-                <img :src="item.imagePath" class="item-image" />
-                <span>{{ item.itemName }}</span>
-            </div>
-            <label class="user-input-label">Name: </label>
-            <input type="text" class="user-input" />
-            <label class="user-input-label">Phone Number: </label>
-            <input type="text" class="user-input" />
 
-            <button>Submit</button>
+            <table>
+                <tbody>
+                    <tr v-for="(item, index) in orderItemList" :key="item.itemNumber">
+                        <td v-if="index % 2 === 0" @click="selectItem(index)" :class="item.selected ? 'selected' : ''">
+                            <div class="item-select">
+                                <img :src="item.imagePath" class="item-image" />
+                                <span>{{ item.itemName }}</span>
+                            </div>
+                        </td>
+                        <td v-if="index % 2 === 0 && index < orderItemList.length - 1" @click="selectItem(index + 1)"
+                            :class="orderItemList[index + 1].selected ? 'selected' : ''">
+                            <div class="item-select">
+                                <img :src="orderItemList[index + 1].imagePath" class="item-image" />
+                                <span>{{ orderItemList[index + 1].itemName }}</span>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <div id="personal-info">
+                <div class="info-group">
+                    <label class="user-input-label">Name: </label>
+                    <input type="text" class="user-input" />
+                </div>
+                <div class="info-group">
+                    <label class="user-input-label">Phone Number: </label>
+                    <input type="text" class="user-input" />
+                </div>
+
+                <button>Submit</button>
+            </div>
         </div>
     </div>
 </template>
@@ -58,13 +101,48 @@ const orderItemList = $ref([
     flex-direction: column;
 }
 
+table {
+    margin: auto;
+}
+
+td {
+    border: 2px solid black;
+}
+
+td.selected {
+    border: 4px solid green;
+}
+
+#personal-info {
+    margin-top: 50px;
+}
+
+.info-group {
+    width: 60%;
+    margin: auto;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding-bottom: 10px;
+}
+
 .user-input {
-    width: 200px;
+    width: 250px;
     font-size: 30px;
 }
 
+.user-input-label {
+    font-size: 30px
+}
+
 .item-image {
-    width: 100px;
-    height: 100px;
+    width: 200px;
+    height: 200px;
+}
+
+.item-select {
+    display: flex;
+    flex-direction: column;
+    margin: auto;
 }
 </style>
